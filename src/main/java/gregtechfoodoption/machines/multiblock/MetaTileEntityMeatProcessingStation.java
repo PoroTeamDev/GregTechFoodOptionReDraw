85b96ce7fea06bd392a549bfd322bd4f4555cc49
Kirill <kirill_molotov88@mail.ru>
2026-09-02 10:13:40 +0300

    Butchery #2
    Fix Meat Station

--- src/main/java/gregtechfoodoption/machines/multiblock/MetaTileEntityMeatProcessingStation.java
+++ src/main/java/gregtechfoodoption/machines/multiblock/MetaTileEntityMeatProcessingStation.java
@@ -1,52 +1,74 @@
 package gregtechfoodoption.machines.multiblock;
 
+import static gregtechfoodoption.block.GTFOMetaBlocks.GTFO_METAL_CASING;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import gregtech.api.recipes.RecipeMap;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraft.world.World;
+
+import org.jetbrains.annotations.NotNull;
 
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Matrix4;
+import gregtech.api.capability.GregtechTileCapabilities;
+import gregtech.api.capability.IControllable;
+import gregtech.api.capability.impl.MultiblockRecipeLogic;
 import gregtech.api.gui.GuiTextures;
 import gregtech.api.gui.ModularUI;
-import gregtech.api.gui.widgets.ProgressWidget;
-import gregtech.api.gui.widgets.SlotWidget;
+import gregtech.api.gui.resources.TextureArea;
+import gregtech.api.gui.widgets.*;
 import gregtech.api.metatileentity.MetaTileEntity;
 import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
-import gregtech.api.metatileentity.multiblock.IMultiblockPart;
-import gregtech.api.metatileentity.multiblock.RecipeMapMultiblockController;
+import gregtech.api.metatileentity.multiblock.*;
 import gregtech.api.pattern.BlockPattern;
 import gregtech.api.pattern.FactoryBlockPattern;
-import gregtech.api.recipes.RecipeMap;
 import gregtech.api.recipes.builders.SimpleRecipeBuilder;
+import gregtech.api.util.RelativeDirection;
+import gregtech.api.util.TextComponentUtil;
 import gregtech.client.renderer.ICubeRenderer;
-import gregtechfoodoption.block.GTFOMetaBlocks;
+import gregtech.core.sound.GTSoundEvents;
 import gregtechfoodoption.block.GTFOMetalCasing;
 import gregtechfoodoption.client.GTFOClientHandler;
-
-import javax.annotation.Nullable;
-import java.util.List;
+import gregtechfoodoption.client.GTFOGuiTextures;
 
 public class MetaTileEntityMeatProcessingStation extends RecipeMapMultiblockController {
 
-    public static final RecipeMap<SimpleRecipeBuilder> MEAT_PROCESSING_RECIPES = new RecipeMap<>(
+    private static final RecipeMap<SimpleRecipeBuilder> MEAT_PROCESSING_RECIPES = new RecipeMap<>(
             "meat_processing",
-            1,  // 1 входной слот
-            9,  // 9 выходных слотов
-            0,  // 0 флюидных входов
-            0,  // 0 флюидных выходов
+            1,
+            9,
+            0,
+            0,
             new SimpleRecipeBuilder(),
             false
     )
-            .setSlotOverlay(false, false, GuiTextures.IN_SLOT_OVERLAY)   // Вход
-            .setSlotOverlay(true, false, GuiTextures.OUT_SLOT_OVERLAY)   // Выходы
+            .setSlotOverlay(false, false, GuiTextures.IN_SLOT_OVERLAY)
+            .setSlotOverlay(true, false, GuiTextures.OUT_SLOT_OVERLAY)
             .setProgressBar(GuiTextures.PROGRESS_BAR_SLICE, ProgressWidget.MoveType.HORIZONTAL);
+
+    private int processedCount = 0;
+    private int totalProcessed = 0; 
 
     public MetaTileEntityMeatProcessingStation(ResourceLocation metaTileEntityId) {
         super(metaTileEntityId, MEAT_PROCESSING_RECIPES);
+        this.recipeMapWorkable = new MeatProcessingWorkable(this);
     }
 
     @Override
@@ -56,18 +78,18 @@ public class MetaTileEntityMeatProcessingStation extends RecipeMapMultiblockCont
 
     @Override
     protected BlockPattern createStructurePattern() {
-        return FactoryBlockPattern.start()
-                .aisle("XXX", "XYX", "XXX")
+        return FactoryBlockPattern.start(RelativeDirection.FRONT, RelativeDirection.UP, RelativeDirection.RIGHT)
+                .aisle("XXX", "YXX", "XXX")
                 .aisle("XXX", "X#X", "XXX")
                 .aisle("XXX", "XXX", "XXX")
-                .where('X', states(getCasingState()).or(autoAbilities()))
+                .where('X', states(getCasingState()).setMinGlobalLimited(10).or(autoAbilities()))
                 .where('Y', selfPredicate())
                 .where('#', air())
                 .build();
     }
 
     protected IBlockState getCasingState() {
-        return GTFOMetaBlocks.GTFO_METAL_CASING.getState(GTFOMetalCasing.CasingType.BISMUTH_BRONZE_CASING);
+        return GTFO_METAL_CASING.getState(GTFOMetalCasing.CasingType.BISMUTH_BRONZE_CASING);
     }
 
     @Override
@@ -88,44 +110,137 @@ public class MetaTileEntityMeatProcessingStation extends RecipeMapMultiblockCont
     }
 
     @Override
-    protected ModularUI createUI(EntityPlayer entityPlayer) {
-        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 220, 210)
-                .label(10, 5, getMetaFullName())
-                // Вход: 1 слот для туши
-                .widget(new SlotWidget(importItems, 0, 25, 50, true, true)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.IN_SLOT_OVERLAY))
-                // Прогресс-бар (используем SLICE, он есть)
-                .progressBar(recipeMapWorkable::getProgressPercent, 50, 65, 20, 20,
-                        GuiTextures.PROGRESS_BAR_SLICE, ProgressWidget.MoveType.HORIZONTAL)
-                // 9 выходных слотов (3x3) с OUT_SLOT_OVERLAY
-                .widget(new SlotWidget(exportItems, 0, 80, 20, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 1, 100, 20, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 2, 120, 20, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 3, 80, 40, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 4, 100, 40, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 5, 120, 40, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 6, 80, 60, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 7, 100, 60, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .widget(new SlotWidget(exportItems, 8, 120, 60, true, false)
-                        .setBackgroundTexture(GuiTextures.SLOT, GuiTextures.OUT_SLOT_OVERLAY))
-                .bindPlayerInventory(entityPlayer.inventory, GuiTextures.SLOT, 130);
+    protected void addDisplayText(List<ITextComponent> textList) {
+        if (!this.isStructureFormed()) {
+            ITextComponent tooltip = new TextComponentTranslation("gregtech.multiblock.invalid_structure.tooltip");
+            tooltip.setStyle((new Style()).setColor(TextFormatting.GRAY));
+            textList.add((new TextComponentTranslation("gregtech.multiblock.invalid_structure")).setStyle((new Style())
+                    .setColor(TextFormatting.RED).setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, tooltip))));
+        } else {
+            if (!this.recipeMapWorkable.isWorkingEnabled()) {
+                textList.add(TextComponentUtil.translationWithColor(TextFormatting.GRAY, "gregtech.multiblock.work_paused"));
+            } else if (this.recipeMapWorkable.isActive()) {
+                textList.add(TextComponentUtil.translationWithColor(TextFormatting.GRAY, "gregtech.multiblock.running"));
+                int currentProgress = (int) (this.recipeMapWorkable.getProgressPercent() * 100.0D);
+                textList.add(new TextComponentTranslation("gregtech.multiblock.progress", currentProgress));
+            } else {
+                textList.add(TextComponentUtil.translationWithColor(TextFormatting.GRAY, "gregtech.multiblock.idling"));
+            }
 
+            if (this.recipeMapWorkable.isHasNotEnoughEnergy()) {
+                textList.add((new TextComponentTranslation("gregtech.multiblock.not_enough_energy"))
+                        .setStyle((new Style()).setColor(TextFormatting.RED)));
+            }
+
+            textList.add(new TextComponentTranslation("gregtechfoodoption.multiblock.meat_processing.tooltip.count",
+                    this.processedCount));
+        }
+    }
+
+    @Override
+    protected ModularUI createUI(EntityPlayer entityPlayer) {
+        ModularUI.Builder builder = ModularUI.builder(GuiTextures.BACKGROUND, 198, 208);
+
+        builder.image(4, 4, 190, 109, GuiTextures.DISPLAY);
+        builder.label(9, 9, getMetaFullName(), 0xFFFFFF);
+        builder.widget(new AdvancedTextWidget(9, 20, this::addDisplayText, 0xFFFFFF)
+                .setMaxWidthLimit(181)
+                .setClickHandler(this::handleDisplayClick));
+
+        ProgressWidget progressBar = new ProgressWidget(
+                () -> this.recipeMapWorkable.getProgressPercent(),
+                4, 115, 190, 7,
+                GuiTextures.PROGRESS_BAR_SLICE,
+                ProgressWidget.MoveType.HORIZONTAL
+        );
+        builder.widget(progressBar);
+
+        IControllable controllable = getCapability(GregtechTileCapabilities.CAPABILITY_CONTROLLABLE, null);
+        if (controllable != null) {
+            builder.widget(new ImageCycleButtonWidget(173, 183, 18, 18, GuiTextures.BUTTON_POWER,
+                    controllable::isWorkingEnabled, controllable::setWorkingEnabled));
+            builder.widget(new ImageWidget(173, 201, 18, 6, GuiTextures.BUTTON_POWER_DETAIL));
+        }
+
+        builder.widget(new IndicatorImageWidget(174, 93, 17, 17, getLogo())
+                .setWarningStatus(getWarningLogo(), this::addWarningText)
+                .setErrorStatus(getErrorLogo(), this::addErrorText));
+
+        builder.bindPlayerInventory(entityPlayer.inventory, 125);
         return builder.build(getHolder(), entityPlayer);
     }
 
     @Override
-    public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, boolean advanced) {
-        super.addInformation(stack, world, tooltip, advanced);
+    protected @NotNull TextureArea getLogo() {
+        return GTFOGuiTextures.GTFO_LOGO_WORKING;
+    }
+
+    @Override
+    protected @NotNull TextureArea getWarningLogo() {
+        return GTFOGuiTextures.GTFO_LOGO_WARNING;
+    }
+
+    @Override
+    protected @NotNull TextureArea getErrorLogo() {
+        return GTFOGuiTextures.GTFO_LOGO_ERROR;
+    }
+
+    @Override
+    public SoundEvent getBreakdownSound() {
+        return GTSoundEvents.BREAKDOWN_ELECTRICAL;
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound data) {
+        super.writeToNBT(data);
+        data.setInteger("ProcessedCount", processedCount);
+        return data;
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound data) {
+        super.readFromNBT(data);
+        processedCount = data.getInteger("ProcessedCount");
+    }
+
+    @Override
+    public void writeInitialSyncData(PacketBuffer buf) {
+        super.writeInitialSyncData(buf);
+        buf.writeInt(processedCount);
+    }
+
+    @Override
+    public void receiveInitialSyncData(PacketBuffer buf) {
+        super.receiveInitialSyncData(buf);
+        processedCount = buf.readInt();
+    }
+
+    @Override
+    public void receiveCustomData(int dataId, PacketBuffer buf) {
+        super.receiveCustomData(dataId, buf);
+        if (dataId == 600) {
+        }
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
         tooltip.add(I18n.format("gregtechfoodoption.machine.meat_processing_station.tooltip.1"));
         tooltip.add(I18n.format("gregtechfoodoption.machine.meat_processing_station.tooltip.2"));
         tooltip.add(I18n.format("gregtechfoodoption.machine.meat_processing_station.tooltip.3"));
+    }
+
+    private class MeatProcessingWorkable extends MultiblockRecipeLogic {
+
+        public MeatProcessingWorkable(RecipeMapMultiblockController tileEntity) {
+            super(tileEntity);
+        }
+
+        @Override
+        protected void completeRecipe() {
+            super.completeRecipe();
+            processedCount++;
+            markDirty();
+        }
     }
 }
